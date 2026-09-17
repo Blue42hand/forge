30da3d6ae998306d63976ca9cf8b514fec787e66
Blue42hand <flamingheads@gmail.com>
2026-09-16 19:28:20 -0700

    Keep schema version on DecisionContext

--- forge-gui/src/main/java/forge/gamemodes/net/DecisionContextVersion.java
+++
@@ -1,7 +0,0 @@
-package forge.gamemodes.net;
-
-/** Shared schema marker for external decision-context consumers. */
-public final class DecisionContextVersion {
-    private DecisionContextVersion() {}
-    public static final int CURRENT = DecisionContext.SCHEMA_VERSION;
-}
